2e3ffb9037aa936579f71dfd6161f81ae0f2d51c
yehudae <yehudae@mcmaster.ca>
2019-03-13 16:55:41 -0400

    Changes to User ADT

--- DataAlloc/Users.java
+++ DataAlloc/Users.java
@@ -1,15 +1,42 @@
 package DataAlloc;
 
-public class Users {
+public class User implements Comparable<User> {
 	private String username;
-	private String[] animeList;
+	private Integer[] animeList;
 	private int[] eps;
 	private double[] scores;
 	
-	public Users(String username, String[] animeList, int[] eps, double[] scores){
+	public User(String username, Integer[] animeList, int[] eps, double[] scores){
 		this.username = username;
 		this.animeList = animeList;
 		this.eps = eps;
 		this.scores = scores;
-	}	
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public Integer[] getAnimeList() {
+		return animeList;
+	}
+	
+	public double getScores(int key) {
+		return scores[key];            //needs modification
+	}
+	
+	public int compareTo(User that) {
+		int k = 0;                     //represents intended anime
+		if (this.getScores(k) > that.getScores(k))
+			return 1;
+		
+		else if (that.getScores(k) == this.getScores(k)) {
+			if (this.getUsername().compareTo(that.getUsername()) > 0)
+				return 1;
+			else if (this.getUsername().compareTo(that.getUsername()) < 0)
+				return -1;
+			return 0;
+		}
+		return -1;
+	}
 }
